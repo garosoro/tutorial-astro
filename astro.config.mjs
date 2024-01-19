44a4745f741790cc808d822e4c8c50d56b23d015
garosoro <gabrielr.sotorojas@gmail.com>
2024-01-18 20:13:56 -0500

    Proyecto Finalizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://my-astro-garosoro.netlify.app/"
+  site: "https://my-astro-garosoro.netlify.app/",
+  integrations: [preact()]
 });
